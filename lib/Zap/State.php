5986204ed234fc7b33c0b3163fbad307f29e203a
Brad Griffith <bgriffith@bgriffith-t400s.(none)>
2011-05-07 17:04:10 -0500

    Port the three interfaces involved in the proof of concept files.

--- lib/Zap/State.php
+++ lib/Zap/State.php
@@ -1,7 +1,5 @@
 <?php
 
-/* vim: set noexpandtab tabstop=4 shiftwidth=4 foldmethod=marker: */
-
 /**
  * Interface for controls that can store and restore their state.
  *
@@ -11,37 +9,32 @@
  */
 interface Zap_State
 {
-	// {{{ public function setState()
+    /**
+     * Set the state of the control
+     *
+     * Used to set the current state of the control back to a stored state.
+     * This implementation of this method should correspond to the
+     * implementation of getState(). Sub-classes should override and implement
+     * this method to store their state.
+     *
+     * @param mixed $state The state to load into the control.
+     *
+     * @return $this
+     */
+    public function setState($state);
 
-	/**
-	 * Set the state of the control
-	 *
-	 * Used to set the current state of the control back to a stored state.
-	 * This implementation of this method should correspond to the
-	 * implementation of getState(). Sub-classes should override and implement
-	 * this method to store their state.
-	 *
-	 * @param mixed $state The state to load into the control.
-	 */
-	public function setState($state);
-
-	// }}}
-	// {{{ public function getState()
-
-	/**
-	 * Get the state of the control
-	 *
-	 * Used to remember the current state of the control. For example,
-	 * {@link SwatEntry} implements this method to return its $value property,
-	 * but can return any variable type, including array, that represents the
-	 * control's current state. Sub-classes should override and implement this
-	 * method to store their state.
-	 *
-	 * @return mixed The current state of the control.
-	 */
-	public function getState();
-
-	// }}}
+    /**
+     * Get the state of the control
+     *
+     * Used to remember the current state of the control. For example,
+     * {@link SwatEntry} implements this method to return its $value property,
+     * but can return any variable type, including array, that represents the
+     * control's current state. Sub-classes should override and implement this
+     * method to store their state.
+     *
+     * @return mixed The current state of the control.
+     */
+    public function getState();
 }
 
 
--- lib/Zap/Titleable.php
+++ lib/Zap/Titleable.php
@@ -1,7 +1,5 @@
 <?php
 
-/* vim: set noexpandtab tabstop=4 shiftwidth=4 foldmethod=marker: */
-
 /**
  * Objects that are titleable have a title that may be gotten
  *
@@ -12,28 +10,21 @@
  */
 interface Zap_Titleable
 {
-	// {{{ public function getTitle()
+    /**
+     * Gets the title of this object
+     *
+     * @return string the title of this object.
+     */
+    public function getTitle();
 
-	/**
-	 * Gets the title of this object
-	 *
-	 * @return string the title of this object.
-	 */
-	public function getTitle();
-
-	// }}}
-	// {{{ public function getTitleContentType()
-
-	/**
-	 * Gets the content-type of the title of this object
-	 *
-	 * @return string the content-type of the title of this object. Returns
-	 *                 'text/xml' for XHTML fragments and 'text/plain' for
-	 *                 plain text.
-	 */
-	public function getTitleContentType();
-
-	// }}}
+    /**
+     * Gets the content-type of the title of this object
+     *
+     * @return string the content-type of the title of this object. Returns
+     *                 'text/xml' for XHTML fragments and 'text/plain' for
+     *                 plain text.
+     */
+    public function getTitleContentType();
 }
 
 
